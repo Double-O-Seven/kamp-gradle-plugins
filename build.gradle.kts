d662b505bc7fbf7a06d1331d3272d21d40d849df
leadrian <thewishwithin@gmail.com>
2019-02-09 01:54:01 +0100

    Added travis integration and publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,14 @@ buildscript {
 }
 
 plugins {
-    java
     kotlin("jvm") version "1.3.11"
+    java
+    `java-library`
     maven
+    `maven-publish`
+    signing
+    `build-scan`
+    id("org.jetbrains.dokka") version "0.9.17"
     id("com.palantir.git-version") version "0.12.0-rc2"
 }
 
@@ -45,6 +50,16 @@ version = gitVersion()
 
 group = "ch.leadrian.samp.kamp"
 
+tasks.register<Jar>("sourcesJar") {
+    from(sourceSets.main.get().allSource)
+    archiveClassifier.set("sources")
+}
+
+tasks.register<Jar>("javadocJar") {
+    from(tasks.dokka)
+    archiveClassifier.set("javadoc")
+}
+
 tasks {
     compileKotlin {
         sourceCompatibility = "1.8"
@@ -63,4 +78,65 @@ tasks {
     test {
         useJUnitPlatform()
     }
+
+    dokka {
+        reportUndocumented = false
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+
+            pom {
+                name.set("Kamp Gradle Plugins")
+                description.set("Gradle plugins for Kamp")
+                url.set("https://github.com/Double-O-Seven/kamp-gradle-plugins")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Double-O-Seven")
+                        name.set("Adrian-Philipp Leuenberger")
+                        email.set("thewishwithin@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/Double-O-Seven/kamp-gradle-plugins.git")
+                    developerConnection.set("scm:git:ssh://github.com/Double-O-Seven/kamp-gradle-plugins.git")
+                    url.set("https://github.com/Double-O-Seven/kamp-gradle-plugins")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials {
+                val ossrhUsername: String? by extra
+                val ossrhPassword: String? by extra
+                username = ossrhUsername
+                password = ossrhPassword
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
 }
